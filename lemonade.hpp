0c5cbc83d7642eaacb4035725b73496f6ac3b9a4
FutureMilk <vshipman@umich.edu>
2025-01-07 17:10:23 -0500

    every order has a customer, instead od rhe other way around

--- lemonade.hpp
+++ lemonade.hpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <vector> 
 #include <queue>
-#include <map>
+#include <unordered_map>
 
 
 using namespace std;
+
+struct Ingredient {
+    string name;
+    int32_t cost;
+};
 
 struct Order {
     int64_t id = -1;
     int16_t cupsOfLemonade;
     double price;
+    Customer customer;
 };
 
 struct Customer {
-    bool frugal;  //customer has a 50% chance of paying for their order
+    // bool frugal;  //customer has a 50% chance of paying for their order
     string name;
-    Order order;
-
     int64_t rewardsId = -1;    //if id is -1, they are not a rewards member
-
 };
 
 //declare custom constructor
 struct CompareOrders {
-    bool operator()(Customer const& a, Customer const& b){
-
-        //if customer.order id > customer.order id do what
-
-        // if 
+    bool operator()(Order const& a, Order const& b){
+        return a.id < b.id; //TODO: will there be a cse of the same id?
     }
 };
 
@@ -35,12 +35,15 @@ struct LemonadeStand {
     double balance;
     string businessName;
 
-    priority_queue<Customer> customers;
-
-
-
+    priority_queue<Order, vector<Order>, CompareOrders> orders;
+    unordered_map<Ingredient, int32_t> inventory;  //what the ing is and qty
+    //Transaction, vector<Transaction>, CompareTransactions>
 
 };
 
+//random algo for creation of a customer
+Customer birthCustomer(LemonadeStand &ls) {
 
-//use a random algo to produce x amount of customers that have a rewards plan
+}
+
+
